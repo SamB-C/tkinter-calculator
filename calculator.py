7f9fb7ff466b73ac5d78e7d706132ea20607b71f
SamB-C <samson_bc@icloud.com>
2022-04-09 10:56:41 +0100

    Integrate new commands
    Successful integration of the new commands from commands2.py. % is only thing that can be a litle buggy.

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from commands import *
+from commands2 import button_clicked
 
 def main():
 
@@ -14,47 +14,48 @@ def main():
 
     #CREATE BUTTONS
     # Returns the function to be passed in as a commmand to the Buttons
-    def get_number(num):
-        return lambda: button_click(num)
+    def get_command(item):
+        print('Getting command: ' + item, '\tType:', type(item))
+        return lambda: button_clicked(item, result)
 
     # Create Brackets and Clear (Row 1)
-    button_open_brackets = Button(root, text='(', command=open_bracket, **regular_button_design)
-    button_close_brackets = Button(root, text=')', command=close_bracket, **regular_button_design)
+    button_open_brackets = Button(root, text='(', command=get_command('('), **regular_button_design)
+    button_close_brackets = Button(root, text=')', command=get_command(')'), **regular_button_design)
     button_open_brackets.grid(row=1, column=0)
     button_close_brackets.grid(row=1, column=1)
 
-    button_clear = Button(root, text='CE', command=clear, **regular_button_design)
+    button_clear = Button(root, text='CE', command=get_command('CE'), **regular_button_design)
     button_clear.grid(row=1, column=3)
 
-    button_convert_percentage = Button(root, text='%', command=lambda: button_operation('%'), **regular_button_design)
+    button_convert_percentage = Button(root, text='%', command=get_command('%'), **regular_button_design)
     button_convert_percentage.grid(row=1, column=2)
 
 
     # Create and position numbers 0 - 9
-    numbers = [Button(root, text=i, command=get_number(i), **regular_button_design) for i in range(1, 10)]
+    numbers = [Button(root, text=i, command=get_command(str(i)), **regular_button_design) for i in range(1, 10)]
 
     for index, number_button in enumerate(numbers[::-1]):
         column = 2 - (index % 3)
         row = (index // 3) + 2
         number_button.grid(row=row, column=column)
-    numbers.insert(0, Button(root, text='0', **regular_button_design, command=lambda: button_click(0)))
+    numbers.insert(0, Button(root, text='0', **regular_button_design, command=get_command('0')))
     numbers[0].grid(row=5, column=0)
 
 
 
     # Create buttons for = and . (Row 5)
-    button_equals = Button(root, text='=', **regular_button_design, command=button_equal)
+    button_equals = Button(root, text='=', **regular_button_design, command=get_command('='))
     button_equals.grid(row=5, column=2,)
 
-    button_floating_point = Button(root, text='.', command=lambda: button_click('.'), **regular_button_design)
+    button_floating_point = Button(root, text='.', command=get_command('.'), **regular_button_design)
     button_floating_point.grid(row=5, column=1)
 
 
     # Create + - * / (Column 3)
-    button_add = Button(root, text='+', command=lambda: button_operation('+'), **regular_button_design)
-    button_subtract = Button(root, text='-', command=lambda: button_operation('-'), **regular_button_design)
-    button_multiply = Button(root, text='*', command=lambda: button_operation('*'), **regular_button_design)
-    button_divide = Button(root, text='/', command=lambda: button_operation('/'), **regular_button_design)
+    button_add = Button(root, text='+', command=get_command('+'), **regular_button_design)
+    button_subtract = Button(root, text='-', command=get_command('-'), **regular_button_design)
+    button_multiply = Button(root, text='*', command=get_command('*'), **regular_button_design)
+    button_divide = Button(root, text='/', command=get_command('/'), **regular_button_design)
     button_add.grid(row=2, column=3)
     button_subtract.grid(row=3, column=3)
     button_multiply.grid(row=4, column=3)
--- commands2.py
+++ commands2.py
@@ -1,5 +1,15 @@
+from tkinter import END
+
+from pandas import array
+
+def clear(display_to_clear, *lists_to_clear):
+    display_to_clear.delete(0, END)
+    for lst in lists_to_clear:
+        lst.clear()
+
 def append_and_display(item_to_add, array_to_display, item_to_display_to):
     array_to_display.append(item_to_add)
+    print('Display', array_to_display)
     item_to_display_to.delete(0, END)
     item_to_display_to.insert(0, ''.join(array_to_display))
 
@@ -8,14 +18,18 @@ evaluation_expression = []
 expression_to_display = []
 
 def button_clicked(item_selected, display, evaluation_expression=evaluation_expression, expression_to_display=expression_to_display):
-    alternate_displays = {'²': '**2', '√': '**0.5'}
-    if item_selected not in ['²', '√', '=']:
+    alternate_displays = {'²': '**2', '√': '**0.5', '%': '/100'}
+    if item_selected not in alternate_displays.keys() and item_selected not in ['=', 'CE']:
         evaluation_expression.append(item_selected)
+        append_and_display(item_selected, expression_to_display, display)
     elif item_selected in alternate_displays.keys():
         evaluation_expression.append(alternate_displays[item_selected])
         append_and_display(item_selected, expression_to_display, display)
     elif item_selected == '=':
-        total = eval(''.join(evaluation_expression))
-        evaluation_expression = total
+        total = str(eval(''.join(evaluation_expression)))
+        clear(display, evaluation_expression, expression_to_display)
+        evaluation_expression.append(total)
         append_and_display(total, expression_to_display, display)
+    elif item_selected == 'CE':
+        clear(display, evaluation_expression, expression_to_display)
 
